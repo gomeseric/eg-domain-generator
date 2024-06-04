d3b8edd2dfd9d654fa45a97aa0e7e961f805cad9
gomeseric <gomesericson.a@gmail.com>
2024-06-04 00:00:26 +0000

    completed

--- src/app.js
+++ src/app.js
@@ -9,3 +9,17 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+let pronous = ["the", "my", "our"];
+let adjs = ["amazing", "sad", "beautiful"];
+let nouns = ["city", "dog", "house"];
+
+const domains = [];
+
+for (const p of pronouns) {
+  for (const a of adjs) {
+    for (const n of nouns) {
+      const d = p + a + n + ".com";
+      domains.push(d);
+    }
+  }
+}
